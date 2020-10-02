ab7bbcc5d1ace230ee16863a4bc93f0aca4f2f6a
Petri Poutiainen <Ppoutiainen92@gmail.com>
2020-10-02 18:45:44 +0300

    Registeration form added

--- lainausjarjestelma/lainausjarjestelma/settings.py
+++ lainausjarjestelma/lainausjarjestelma/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'books.apps.BooksConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fi'
 
 TIME_ZONE = 'UTC'
 
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
--- lainausjarjestelma/users/views.py
+++ lainausjarjestelma/users/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 
 def register(request):
-    return render(request, "users/register.html")
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Käyttäjä {username} luotu!")
+            return redirect("home")
+    else:
+        form = UserRegisterForm()
+    return render(request, "users/register.html", {"form": form})
