34d6eea5f6f660d93af475572295458c9422e48d
Petri Poutiainen <Ppoutiainen92@gmail.com>
2020-10-09 13:45:24 +0300

    Updating Profile model, form and added signals.py for it

--- lainausjarjestelma/users/models.py
+++ lainausjarjestelma/users/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
+    phone = models.CharField(max_length=30)
 
     def __str__(self):
         return self.user.username
