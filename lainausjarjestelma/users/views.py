091cacb2fd9cce85ef57a939e4cb2ef470c654c0
Petri Poutiainen <Ppoutiainen92@gmail.com>
2020-10-09 13:56:01 +0300

    Front end added to Profile form

--- lainausjarjestelma/users/views.py
+++ lainausjarjestelma/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -19,4 +19,8 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, "users/profile.html")
+    p_form = ProfileUpdateForm(instance=request.user)
+    context = {
+        "p_form": p_form
+    }
+    return render(request, "users/profile.html", context)
