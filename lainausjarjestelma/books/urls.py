7193dc1383b110f15bacf1aeccde41fbb9a2be4c
Petri Poutiainen <Ppoutiainen92@gmail.com>
2020-10-12 18:57:55 +0300

    book_list working

--- lainausjarjestelma/books/urls.py
+++ lainausjarjestelma/books/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
-
+from books.views import BookListView
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', BookListView.as_view(), name='book-list'),
 ]
--- lainausjarjestelma/books/views.py
+++ lainausjarjestelma/books/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from books.models import Book
+from django.views import generic
+from django.utils import timezone
 # Create your views here.
 
 
-def home(request):
-    return render(request, "books/home.html")
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/book_list.html'
+
+    def showbooks(request):
+        allbooks = Book.object.all()
+        context = {'allbooks': allbooks}
+        return render(request, 'books/book_list.html', context)
