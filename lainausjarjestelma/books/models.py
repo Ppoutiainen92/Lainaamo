7a34aeee585e4383614f2129a95a195114caf815
Petri Poutiainen <Ppoutiainen92@gmail.com>
2020-10-12 20:06:53 +0300

    Image resize

--- lainausjarjestelma/books/models.py
+++ lainausjarjestelma/books/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 import datetime
 from django.utils import timezone
+from PIL import Image
+from django_resized import ResizedImageField
 
 # Create your models here.
+MAX_HEIGHT = 250
+MAX_WIDTH = 250
 
 
 class Book(models.Model):
@@ -17,8 +21,16 @@ class Book(models.Model):
     amount = models.IntegerField(default=0, blank=True)
     rental_duration = models.IntegerField(blank=True, null=True)
     expire_date = models.DateTimeField(blank=True, null=True)
-    thumbnail = models.ImageField(
-        default="default.jpg", upload_to="book_pics", blank=True)
+    thumbnail = ResizedImageField(
+        size=[MAX_WIDTH, MAX_HEIGHT], default="default.jpg", upload_to="book_pics", blank=True)
+
+    # def save(self):
+    #     super(Book, self).save
+    #     img = Image.open(self.thumbnail.path)
+    #     if img.height > max_height or img.width > max_width:
+    #         output_size = (max_height, max_width)
+    #         img.thumbnail(output_size)
+    #         img.save(self.thumbnail.path)
 
     def __str__(self):
         return self.book_title + " " + self.author
