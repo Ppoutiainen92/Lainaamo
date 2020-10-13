ad0f5871a6d38b82c952b23582c8177520fc6778
Petri Poutiainen <Ppoutiainen92@gmail.com>
2020-10-13 13:28:50 +0300

    bookdetails working

--- lainausjarjestelma/books/urls.py
+++ lainausjarjestelma/books/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-from books.views import BookListView
+from books.views import BookListView, BookDetailView
 
 urlpatterns = [
     path('', BookListView.as_view(), name='book-list'),
+    path('<int:pk>', BookDetailView.as_view(), name='book-detail')
 ]
--- lainausjarjestelma/books/views.py
+++ lainausjarjestelma/books/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from books.models import Book
 from django.views import generic
 from django.utils import timezone
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 
@@ -10,9 +11,33 @@ class BookListView(generic.ListView):
     model = Book
     context_object_name = 'books'
     template_name = 'books/book_list.html'
-    paginate_by = 4
+    paginate_by = 9
 
     # def showbooks(request):
     #     allbooks = Book.object.all()
     #     context = {'allbooks': allbooks}
     #     return render(request, 'books/book_list.html', context)
+
+
+# class BookDetailView(generic.DetailView):
+#     # model = Book
+#     context_object_name = 'book'
+#     template_name = 'books/detail.html'
+
+#     def get_object(self):
+#         id_ = self.kwargs.get("id")
+#         return get_object_or_404(Book, id=id_)
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+   #context_object_name = 'book_detail'
+   #template_name = 'books/book_detail.html'
+
+    def book_detail_view(request, primary_key):
+        try:
+            book = Book.objects.get(pk=primary_key)
+        except Book.DoesNotExist:
+            raise Http404('Book does not exist')
+        else:
+            return render(request, 'books/detail.html', context={'book': book})
