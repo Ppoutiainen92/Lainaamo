e1f4694afc6d5e2b49f7ba7467afc1e80e9f0ae1
Petri Poutiainen <Ppoutiainen92@gmail.com>
2020-10-14 12:14:53 +0300

    libraries init

--- lainausjarjestelma/lainausjarjestelma/settings.py
+++ lainausjarjestelma/lainausjarjestelma/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'libraries.apps.LibrariesConfig',
     'users.apps.UsersConfig',
     'books.apps.BooksConfig',
     'crispy_forms',
