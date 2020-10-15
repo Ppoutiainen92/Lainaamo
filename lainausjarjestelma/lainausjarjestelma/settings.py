510a4be5482398a7a333bd961ec07dde217752d9
Petri Poutiainen <Ppoutiainen92@gmail.com>
2020-10-15 13:41:54 +0300

    cart init

--- lainausjarjestelma/lainausjarjestelma/settings.py
+++ lainausjarjestelma/lainausjarjestelma/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cart.apps.CartConfig',
     'libraries.apps.LibrariesConfig',
     'users.apps.UsersConfig',
     'books.apps.BooksConfig',
--- lainausjarjestelma/lainausjarjestelma/urls.py
+++ lainausjarjestelma/lainausjarjestelma/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
     path('profile/', user_views.profile, name="profile"),
     path('library/', include('libraries.urls'), name="library"),
+    path('cart/', include('cart.urls'), name="cart")
 
 
 ]
