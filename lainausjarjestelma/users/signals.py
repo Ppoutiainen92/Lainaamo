396c1769f42ffeca19a102744d9c0afe7f5739d3
Petri Poutiainen <Ppoutiainen92@gmail.com>
2020-10-16 19:14:34 +0300

    Order model is created when user is

--- lainausjarjestelma/users/signals.py
+++ lainausjarjestelma/users/signals.py
@@ -3,6 +3,7 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
+from cart.models import Order
 
 
 @receiver(post_save, sender=User)
@@ -10,8 +11,14 @@ def create_profile(sender, instance, created, **kwargs):
     """Create always blank profile when user is created"""
     if created:
         Profile.objects.create(user=instance)
+        Order.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+# @receiver(post_save, sender=User)
+# def save_profile(sender, instance, **kwargs):
+#     instance.order.save()
